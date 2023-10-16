373154bd3cfea5f47986b925e6f19110168f1b07
Naylek_ <69121494+TTVNaylek@users.noreply.github.com>
2023-10-16 16:02:53 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-//This file is part of TpStl
-//TpStl is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-//TpStl is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//You should have received a copy of the GNU General Public License along with TpStl If not, see <https://www.gnu.org/licenses/>
+//This file is part of TpSocket
+//TpSocket is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//TpSocket is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//You should have received a copy of the GNU General Public License along with TpSocket If not, see <https://www.gnu.org/licenses/>
 using System;
 using System.Collections.Generic;
 using System.Linq;
